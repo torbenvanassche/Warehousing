24cafa17993d3fb6dcd996fb232d636a92defeb3
Torben Van Assche <torbenvanassche.anime@gmail.com>
2024-05-14 23:30:27 +0200

    added textures and emission fix for new assets

--- game_mechanics/global_scripts/input_item_buffered_display.gd
+++ game_mechanics/global_scripts/input_item_buffered_display.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var spawner: ItemGenerator = $"./../";
 var spawned_item_buffer: Inventory
 var spawn_locations: Array[Node3D] = [];
+@export var random_rotation: bool = false;
 
 func _ready():
 	var max_item_buffer = get_child_count();
@@ -17,7 +18,10 @@ func _on_signal(item: Dictionary):
 	if spawned_item_buffer.add_item(item, 1, false, true) == 0:
 		var rnd_location: Node3D = handle_random_location();
 		var instance: Node3D = ItemManager.get_scene(item).instantiate();
-		rnd_location.add_child(instance)
+		rnd_location.add_child(instance);
+		
+		if random_rotation:
+			instance.rotate_y(deg_to_rad(randi_range(-180, 180)))
 		pass
 		
 #this code prevents taking locations that are already in use
